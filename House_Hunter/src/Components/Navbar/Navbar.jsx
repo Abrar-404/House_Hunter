3ba72d400767c7ceabc55801e9833fe6a013d5da
Sahil Abrar <zeroxkhan2@gmail.com>
2024-01-23 00:42:11 +0600

    added sticky

--- House_Hunter/src/Components/Navbar/Navbar.jsx
+++ House_Hunter/src/Components/Navbar/Navbar.jsx
@@ -22,8 +22,8 @@ const Navbar = () => {
   );
 
   return (
-    <div>
-      <div className="navbar shadow-lg sticky top-0 z-50 rounded-xl ">
+    <div className="sticky top-0 z-50">
+      <div className="navbar shadow-lg bg-white ">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
