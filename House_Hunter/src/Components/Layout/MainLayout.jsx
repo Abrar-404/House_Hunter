f18a4602ee2cf21b77728ba5aa69ec5ea9f9869d
Sahil Abrar <zeroxkhan2@gmail.com>
2024-01-22 17:48:49 +0600

    added navbar

--- House_Hunter/src/Components/Layout/MainLayout.jsx
+++ House_Hunter/src/Components/Layout/MainLayout.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from 'react-router-dom';
+import Navbar from '../Navbar/Navbar';
 
 const MainLayout = () => {
   return (
     <div>
-      <Outlet></Outlet>
+      <div>
+        <Navbar></Navbar>
+        <Outlet></Outlet>
+      </div>
     </div>
   );
 };
